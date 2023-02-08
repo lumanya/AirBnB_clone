eeaf55218e288f5a0320af5471a2296f421a5d80
lumanya <shaibulumanya@gmail.com>
2023-02-08 17:59:00 +0300

    add file that passes pycodestyle checks

---
+++ pycode_style.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+ pycode_style is the module that contains HelloWord class
+ That contains hello function
+ print AirBnB project on stdout which is pycodestyle complianat
+"""
+
+
+class HelloWord:
+    """
+     Hello Word class contains hello function
+     Method:
+      hello(self)
+    """
+
+    def hello(self):
+        """ print AirBnB project on stdout """
+        print("AirBnB project console")
