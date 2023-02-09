68837ceda6a094f0960c4f7c161dd7584ac4f47b
lumanya <shaibulumanya@gmail.com>
2023-02-09 10:37:57 +0300

    add base class file

---
+++ models/base_model.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""
+ base_model is the module that contains BaseModel class
+ BaseModel class is the base class in which all other class will inherit from
+ this class
+"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+     BaseModel is the class in which all other cas inherit from this
+
+     Methods:
+      __str__(self)
+      to_dict(self)
+    """
+
+    def __init__(self):
+        """
+        Initialize instance of the class
+
+        Args:
+        id (str): string-assign with an uuid when an instance is created
+        created_at (str): datetime-assign with the current datetime when an
+                         an instance is created
+        updated_at (str): datetime-assign with the current datetime when an
+                          an instance is created and is updated every time
+                         we change our obejcts
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+
+    def __str__(self):
+        """ print [<class name>] (<self.id>) <self.__dict__ of instance """
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
+                                     self.__dict__)
+
+    def save(self):
+        """
+           updates the public instance attributes  updated_at with currnet
+           with the current date time
+        """
+        self.updated_at = datetime.today()
+
+    def to_dict(self):
+        """
+         returns d dict values containg all keys/values of __dict__ of the
+         instance
+`        """
+        dict = self.__dict__
+        dict['__class__'] = self.__class__.__name__
+        dict['created_at'] = dict['created_at'].isoformat()
+        dict['updated_at'] = dict['updated_at'].isoformat()
+        return dict
