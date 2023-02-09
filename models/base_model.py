a17cd7eb4fa714aadbf63b2a07570832baf596ee
lumanya <shaibulumanya@gmail.com>
2023-02-09 12:03:05 +0300

    add **kwargs to create BaseModel from it

--- models/base_model.py
+++ models/base_model.py
@@ -28,10 +28,18 @@ class BaseModel:
         updated_at (str): datetime-assign with the current datetime when an
                           an instance is created and is updated every time
                          we change our obejcts
+        **kwargs (dict): contains attributs that create new instance
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+                if key == 'created_at' or key == 'updated_at':
+                    setattr(self, key, datetime.fromisoformat(value))
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """ print [<class name>] (<self.id>) <self.__dict__ of instance """
