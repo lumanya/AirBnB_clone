a3ddcf562d86c1aa65da4d347d2bcadbf96fd9a0
lumanya <shaibulumanya@gmail.com>
2023-02-12 23:01:43 +0300

    add console file

---
+++ console.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Command interpreter for HBNB
+    """
+    prompt = '(hbnb) '
+
+    # The do_quit and do_EOF methods are used to exit the program
+    def do_quit(self, line):
+        """Quit command to exit the program"""
+        return True
+
+    def do_EOF(self, line):
+        """EOF command to exit the program"""
+        return True
+
+    # An empty line + ENTER shouldn't execute anything
+    def emptyline(self):
+        """Do nothing on an empty input line"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
