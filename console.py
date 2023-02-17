64e16a789440f3b5848c16922d3c35ec91c26c97
lumanya <shaibulumanya@gmail.com>
2023-02-17 15:59:53 +0300

    added new line fater documentaion

--- console.py
+++ console.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 import cmd
 
 
@@ -8,18 +8,16 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = '(hbnb) '
 
-    # The do_quit and do_EOF methods are used to exit the program
-    def do_quit(self, line):
-        """Quit command to exit the program"""
+    def do_quit(self, arg):
+        'Quit command to exit the program\n'
         return True
 
-    def do_EOF(self, line):
-        """EOF command to exit the program"""
+    def do_EOF(self, arg):
+        'EOF command to exit the program\n'
         return True
 
-    # An empty line + ENTER shouldn't execute anything
     def emptyline(self):
-        """Do nothing on an empty input line"""
+        """Do nothing on empty input line"""
         pass
 
 
