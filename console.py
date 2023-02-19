eff44ecc9549a944f82fce3439f971793e87e67c
lumanya <shaibulumanya@gmail.com>
2023-02-19 05:36:20 +0300

    add console file

--- console.py
+++ console.py
@@ -4,6 +4,8 @@
 """
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
@@ -23,6 +25,57 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing on empty input line"""
         pass
+
+    def do_create(self, cls):
+        '''
+        Creates a new instance of BaseModel and save it it to JSON file and
+        print id
+        '''
+        if cls == "":
+            print("** class name missing **")
+            return
+        if cls not in ['BaseModel']:
+            print("** class doesn't exist **")
+            return
+        if cls == 'BaseModel':
+            self.new_obj = BaseModel()
+            self.new_obj.save()
+            print(self.new_obj.id)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance based on the class
+        name and id."""
+        args = arg.split()
+        if not arg:
+            print("** class name missing **")
+            return
+        elif args[0] not in ['BaseModel']:
+            print("** class doesn't exist **")
+            return
+        elif len(args) < 2:
+            print("** instance id missing **")
+            return
+        objs = storage.all()
+        key = args[0] + '.' + args[1]
+        if key not in objs:
+            print("** no instance found **")
+            return
+        print(objs[key])
+
+    def do_all(self, args):
+        """ prints all string repersenation of all instances based or not on the
+        class name. Ex all BaseModel or all .
+        """
+        if not args:
+            print([str(obj) for obj in storage.all().values()])
+            return
+        else:
+            args_list = args.split()
+            if args_list[0] not in ['BaseModel']:
+                print("** class doesn't exists **")
+                return
+            else:
+                print([str(obj) for obj in storage.all().values()])
 
 
 if __name__ == '__main__':
